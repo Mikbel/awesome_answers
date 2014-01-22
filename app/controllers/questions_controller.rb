53fdf9ed8483db9c8a022018ed30164cbc2125e2
Mohamed <mohamed@mohamed-ThinkPad-T530.(none)>
2014-01-21 19:32:42 -0800

    add bootstrap and improve look and feel

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,16 @@
 class QuestionsController < ApplicationController
+  def index
+  end
+  def new
+    @question = Question.new
+  end
+  def create
+    @question = Question.new(params.require(:question).  
+   permit([:title, :body]))
+      @question.save
+redirect_to @question
+  end
+  def show
+    @question = Question.find(params[:id])
+  end
 end
